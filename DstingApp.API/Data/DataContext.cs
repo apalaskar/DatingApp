361f99ebabad2b5c81e1b2df036085b753e8a3af
pabhijeet39 <pabhijeet39@gmail.com>
2020-05-03 22:13:54 +0530

    initial commit

---
+++ DstingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DstingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DstingApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options):base (options) {}
+        public DbSet <Value> Values { get; set; }
+    }
+}
