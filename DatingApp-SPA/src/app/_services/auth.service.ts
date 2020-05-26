2fdef763461bda1eaca872b3f3ace379f7ab9590
pabhijeet39 <pabhijeet39@gmail.com>
2020-05-27 00:37:05 +0530

    End of Section 9

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
-import{JwtHelperService} from '@auth0/angular-jwt'
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  baseUrl = "http://localhost:5000/api/auth/";
+  baseUrl = environment.apiUrl + "auth/";
   jwtHelper = new JwtHelperService();
-  decodedToken:any;
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-   return this.http.post(this.baseUrl + "login", model)
-    .pipe(
+    return this.http.post(this.baseUrl + "login", model).pipe(
       map((response: any) => {
         const user = response;
         if (user) localStorage.setItem("token", user.token);
@@ -24,12 +24,12 @@ export class AuthService {
     );
   }
 
-  register(model:any){
-    return this.http.post(this.baseUrl+'register',model);
+  register(model: any) {
+    return this.http.post(this.baseUrl + "register", model);
   }
 
-  loggedIn(){
-    const token = localStorage.getItem('token');
+  loggedIn() {
+    const token = localStorage.getItem("token");
     return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,8 +2,11 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages/messages.component';
 import { ListsComponent } from './lists/lists/lists.component';
-import { MemberListComponent } from './member-list/member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { AuthGuard } from "./_guards/auth.guard";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberDetailResolver } from "./_resolver/member-detail.resolver";
+import { MemberListResolver } from "./_resolver/member-list.resolver";
 
 export const  appRoutes:Routes = [
     {path:'',component:HomeComponent}, //Home if only http://localhost:4200/ is pasted in browser & user is not logged in
@@ -12,7 +15,8 @@ export const  appRoutes:Routes = [
         runGuardsAndResolvers:"always",
         canActivate:[AuthGuard],
         children:[ // to all children we specify AuthGuard
-            {path:'members',component:MemberListComponent },
+            {path:'members',component:MemberListComponent,resolve:{users:MemberListResolver} },
+            {path:'members/:id',component:MemberDetailComponent ,resolve:{user:MemberDetailResolver}},
             {path:'messages',component:MessagesComponent},
             {path:'lists',component:ListsComponent},
         ]
